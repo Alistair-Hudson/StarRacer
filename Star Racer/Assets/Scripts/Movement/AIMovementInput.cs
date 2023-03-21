8c4f8a5919e95adb1d27db34d9876d3f27eb599c
AlistairHudson <alistairh@oversight-ar.com>
2023-03-21 13:38:22 +0200

    AI script running needs some inprovement

--- Star Racer/Assets/Scripts/Movement/AIMovementInput.cs
+++ Star Racer/Assets/Scripts/Movement/AIMovementInput.cs
@@ -9,8 +9,10 @@ namespace StarRacer.Movement
     {
         [SerializeField]
         private float minDistanceToTarget = 0.1f;
+        [SerializeField]
+        private float deccelerationChance = 0.5f;
 
-        private Waypoint targetWaypoint;
+        public Waypoint targetWaypoint;
         private Vector3 target;
 
         private void Awake()
@@ -21,7 +23,49 @@ namespace StarRacer.Movement
 
         private void Update()
         {
-            
+            if (Vector3.Distance(target, transform.position) <= minDistanceToTarget)
+            {
+                targetWaypoint = targetWaypoint.NextWaypoint;
+                target = targetWaypoint.GetPoint(Random.Range(-1f, 1), Random.Range(-1f, 1), Random.Range(-1f, 1));
+            }
+
+            transform.LookAt(target);
+            //TODO: find a means of making this work, so the turning looks more natural
+            /*Vector2 targetDir = targetWaypoint.transform.position - transform.position;
+            float yawAngle = Vector3.SignedAngle(targetDir, transform.forward, Vector3.up);
+            if (yawAngle < 0)
+            {
+                TurnLeft();
+            }
+            else if (yawAngle > 0)
+            {
+                TurnRight();
+            }
+            else
+            {
+                ResetModelRoll();
+            }
+
+            float pitchAngle = Vector3.SignedAngle(targetDir, transform.forward, Vector3.right);
+            if (pitchAngle < 0)
+            {
+                PitchDown();
+            }
+            else if (pitchAngle > 0)
+            {
+                PitchUp();
+            }*/
+
+            //Accelerate towards target call random deccelerate
+            if (Random.Range(0f, 1f) < deccelerationChance)
+            {
+                CallDecelerate();
+            }
+            else
+            {
+                CallAccelerate();
+            }
+
         }
     }
 }
