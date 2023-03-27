c1769decb59a7860a3f7abe2673a3a09909854db
AlistairHudson <alistairh@oversight-ar.com>
2023-03-27 16:54:48 +0300

    Changed the turn rate and accelerations to be frame independent

--- Star Racer/Assets/Scripts/Movement/MovementController.cs
+++ Star Racer/Assets/Scripts/Movement/MovementController.cs
@@ -8,12 +8,16 @@ namespace StarRacer.Movement
     public class MovementController : MonoBehaviour
     {
         [SerializeField]
+        [Tooltip("m.s-2")]
         private float acceleration = 1f;
         [SerializeField]
+        [Tooltip("degrees per second")]
         private float pitchRate = 1f;
         [SerializeField]
+        [Tooltip("degrees per second")]
         private float yawRate = 1f;
         [SerializeField]
+        [Tooltip("m.s-1")]
         private float maxSpeed = 50f;
 
         private float currentForwardSpeed = 0;
@@ -30,7 +34,7 @@ namespace StarRacer.Movement
 
         private void Accelerate(bool acclerate)
         {
-            currentForwardSpeed += acclerate ? acceleration : -acceleration;
+            currentForwardSpeed += (acclerate ? acceleration : -acceleration) * Time.deltaTime;
             currentForwardSpeed = Mathf.Clamp(currentForwardSpeed, 0, maxSpeed);
             myRigidBody.velocity = transform.forward * currentForwardSpeed;
         }
@@ -38,14 +42,14 @@ namespace StarRacer.Movement
         private void Pitch(bool pitchForward)
         {
             var rotation = transform.rotation.eulerAngles;
-            rotation += Vector3.right * (pitchForward ? pitchRate : -pitchRate);
+            rotation += Vector3.right * (pitchForward ? pitchRate : -pitchRate) * Time.deltaTime;
             transform.rotation = Quaternion.Euler(rotation);
         }
 
         private void Turn(bool turnRight)
         {
             var rotation = transform.rotation.eulerAngles;
-            rotation += Vector3.up * (turnRight ? yawRate : -yawRate);
+            rotation += Vector3.up * (turnRight ? yawRate : -yawRate) * Time.deltaTime;
             transform.rotation = Quaternion.Euler(rotation);
         }
     }
